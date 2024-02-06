57a2703ccf17b253074f48487c830d46abc67f7c
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2024-02-06 18:30:32 +0200

    date

--- src/components/Blog/PostList.tsx
+++ src/components/Blog/PostList.tsx
@@ -6,11 +6,27 @@ interface Props {
 }
 
 const PostList: React.FC<Props> = ({ posts }) => {
+
+  const convertDate = (dateString: string): string => {
+      const date = new Date(dateString);
+      const day = date.getDate();
+      const month = date.getMonth() + 1;
+      const year = date.getFullYear();
+
+      const formattedDay = day < 10 ? `0${day}` : day;
+      const formattedMonth = month < 10 ? `0${month}` : month;
+
+      return `${formattedDay}.${formattedMonth}.${year}`;
+  }
+
   return (
     <section className='posts_section'>
       {posts.map((post, index) => (
         <div key={index} className='post'>
-          <h1 className='title_post'>{post.title}</h1>
+          <div className='top_post'>
+            <h1 className='title_post'>{post.title}</h1>
+            <h1 className='date_post'>{"· " + convertDate(post.CreatedAt)}</h1>
+          </div>
           <h1 className='description_post'>{post.content}</h1>
         </div>
       ))}
--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -8,68 +8,68 @@ import Pagination from './Pagination';
 import { Post } from '../../types/Post';
 
 const Blog = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-  const [maxId, setMaxId] = useState<number | null>(null);
-  const [cachedPages, setCachedPages] = useState<{ [key: string]: Post[] }>({});
-  const [cachedMaxId, setCachedMaxId] = useState<number | null>(null);
-  const location = useLocation();
-  const { page } = useParams<{ page: string | undefined }>();
+    const [posts, setPosts] = useState<Post[]>([]);
+    const [maxId, setMaxId] = useState<number | null>(null);
+    const [cachedPages, setCachedPages] = useState<{ [key: string]: Post[] }>({});
+    const [cachedMaxId, setCachedMaxId] = useState<number | null>(null);
+    const location = useLocation();
+    const { page } = useParams<{ page: string | undefined }>();
 
-  const perPage = 4;
+    const perPage = 4;
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        if (cachedPages[page || '']) {
-          setPosts(cachedPages[page || '']);
-        } else {
-          const response = await axios.get(`http://localhost:5000/post?page=${page}&perPage=${perPage}`);
-          setPosts(response.data);
-          setCachedPages({ ...cachedPages, [page || '']: response.data });
+    useEffect(() => {
+      const fetchPosts = async () => {
+        try {
+          if (cachedPages[page || '']) {
+            setPosts(cachedPages[page || '']);
+          } else {
+            const response = await axios.get(`http://localhost:5000/post?page=${page}&perPage=${perPage}`);
+            setPosts(response.data);
+            setCachedPages({ ...cachedPages, [page || '']: response.data });
+          }
+        } catch (error) {
+          console.error('Error fetching posts:', error);
         }
-      } catch (error) {
-        console.error('Error fetching posts:', error);
-      }
-    };
+      };
 
-    fetchPosts();
-  }, [page, location, cachedPages]);
+      fetchPosts();
+    }, [page, location, cachedPages]);
 
-  useEffect(() => {
-    const fetchMaxId = async () => {
-      try {
-        if (cachedMaxId !== null) {
-          setMaxId(cachedMaxId);
-        } else {
-          const response = await axios.get('http://localhost:5000/post/maxId');
-          setMaxId(response.data.maxId);
-          setCachedMaxId(response.data.maxId);
+    useEffect(() => {
+      const fetchMaxId = async () => {
+        try {
+          if (cachedMaxId !== null) {
+            setMaxId(cachedMaxId);
+          } else {
+            const response = await axios.get('http://localhost:5000/post/maxId');
+            setMaxId(response.data.maxId);
+            setCachedMaxId(response.data.maxId);
+          }
+        } catch (error) {
+          console.error('Error fetching maxId:', error);
         }
-      } catch (error) {
-        console.error('Error fetching maxId:', error);
-      }
-    };
+      };
 
-    fetchMaxId();
-  }, [cachedMaxId]);
+      fetchMaxId();
+    }, [cachedMaxId]);
 
-  const totalPages = maxId ? Math.abs(maxId / perPage) : 1;
+    const totalPages = maxId ? Math.abs(maxId / perPage) : 1;
 
-  return (
-    <div className='container'>
-      <Header />
-      {posts.length > 0 ? (
-        <div className='content_page'>
-          <PostList posts={posts} />
-          <Pagination totalPages={totalPages} currentPage={parseInt(page || '1')} />
-        </div>
-      ) : (
-        <div className='wrap'>
-          <div className="lds-ripple"><div></div><div></div></div>
-        </div>
-      )}
-    </div>
-  );
+    return (
+      <div className='container'>
+        <Header />
+        {posts.length > 0 ? (
+          <div className='content_page'>
+            <PostList posts={posts} />
+            <Pagination totalPages={totalPages} currentPage={parseInt(page || '1')} />
+          </div>
+        ) : (
+          <div className='wrap'>
+            <div className="lds-ripple"><div></div><div></div></div>
+          </div>
+        )}
+      </div>
+    );
 }
 
 export default Blog;
